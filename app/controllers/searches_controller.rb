1119da0f12168e5375397eac4c8f244e86908e65
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-05 10:00:13 +0000

    [Finish]5-9d

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,13 +2,15 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
   
 	def search
-	  @model = params[:model]
+		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
 		if @model == 'user'
 			@records = User.search_for(@content, @method)
-		else
+		elsif @model == 'book'
 			@records = Book.search_for(@content, @method)
+		elsif @model == 'tag'
+			@records = Tag.search_books_for(@content, @method)
 		end
 	end
 	
