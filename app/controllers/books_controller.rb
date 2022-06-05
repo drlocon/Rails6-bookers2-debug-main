776df211b4241a831b318dfa70a7c5cf1ec80ae9
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-05 08:54:05 +0000

    [Add]カテゴリタグの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
     impressionist(@book, nil, unique: [:ip_address])
+    
+    if params[:tag_id]
+      @tag_list = Tag.all
+      @tag = Tag.find(params[:tag_id])
+      @books = @tag.blogs.public_send.order(time: "DESC").page(params[:page]).per(10)
+      @books_side = Book.public_send.order(time: "DESC")
+    else
+      @tag_list = Tag.all
+      @books = Book.public_send.order(time: "DESC").page(params[:page]).per(10)
+      @books_side = Book.public_send.order(time: "DESC")
+    end
+    respond_to do |format|
+      format.html
+      format.rss { render :layout => false }
+    end
   end
 
   def index
@@ -17,7 +32,9 @@ class BooksController < ApplicationController
   def create
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
+    tag_list = params[:book][:tag_name].delete('').delete(' ').split(nil)
     if @book_new.save
+      @book_new.save_books(tag_list)
       redirect_to book_path(@book_new.id), notice: "You have created book successfully."
     else
       @user = current_user
@@ -28,11 +45,14 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @tag_list = @book.tags.pluck(:tag_name).join(',')
   end
 
   def update
     @book = Book.find(params[:id])
+    tag_list = params[:book][:tag_name].delete('').delete(' ').split(',')
     if @book.update(book_params)
+      @book.save_books(tag_list)
       redirect_to book_path, notice: "You have updated book successfully."
     else
       render "edit"
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,9 @@ class Book < ApplicationRecord
   has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
   
+  has_many :tagmaps, dependent: :destroy
+  has_many :tags, through: :tagmaps
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   
@@ -23,6 +26,22 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content)
     else
       Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+  
+  #タグ
+  def save_books(tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+    
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(tag_name: old_name)
+    end
+    
+    new_tags.each do |new_name|
+      book_tag = Tag.find_or_create_by(tag_name: new_name)
+      self.tags << book_tag
     end
   end
   
