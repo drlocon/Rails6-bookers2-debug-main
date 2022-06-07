7094c39af5be27cc40b00dfbd465950566559066
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-07 08:42:32 +0000

    [Add]グループメンバーの表示

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,7 @@ Rails.application.routes.draw do
   resources :chats, only: [:show, :create]
   
   resources :groups, only: [:index,:show,:edit,:create,:update,:new,:show]
+  get "join" => "groups#join"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
