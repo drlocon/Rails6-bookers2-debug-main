c40c45f4be87b04d5675689e23685884c1593053
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-07 11:30:33 +0000

    [Add]グループメール機能を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "942d10ff0800433d98cd8c3fe69bedb1.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
