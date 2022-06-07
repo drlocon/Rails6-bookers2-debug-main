03a6f82fbe2e1da5b105df3887237ff2a88ad7e5
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-07 09:16:09 +0000

    [Fix]グループメンバーの表示を修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,15 +3,21 @@ class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :group_users, dependent: :destroy
   
+  has_many :users, through: :group_users, source: :user
+
   validates :name, presence: true
   validates :introduction, presence: true
-  
+
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
-  
+
   def is_owned_by?(user)
-    owner_id == user.id
+    owner.id == user.id
+  end
+  
+  def includesUser?(user)
+    group_users.exists?(user_id: user.id)
   end
   
 end
