59ba2293cac2777f0a467042d0771b7086d465cc
drlocon <104292397+drlocon@users.noreply.github.com>
2022-06-07 07:36:06 +0000

    [Finish]5-7c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_041516) do
+ActiveRecord::Schema.define(version: 2022_06_07_073030) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2022_06_07_041516) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
-    t.string "image_id"
     t.integer "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
